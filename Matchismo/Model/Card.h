7d22586166e2393d0fef5a8964096d2e1a02f852
GWseo <seogw@hotmail.com>
2014-03-29 23:19:51 +0900

    new object Card

---
+++ Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by gyuwon on 3/29/14.
+//  Copyright (c) 2014 gyuwon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by gyuwon on 3/29/14.
+//  Copyright (c) 2014 gyuwon. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
